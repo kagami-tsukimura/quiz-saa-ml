4d6cc3ca4bbe370a21f63ca204380e0cd461ba03
Kagami <shimura.kyohei7617@gmail.com>
2024-03-02 12:03:59 +0900

    Initialize repository

---
+++ Quiz.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+col1, col2 = st.columns(2)
+
+col1.title("AWS SAA")
+col1.subheader("Quiz for ML")
+
+
+col2.image("images/mlops.jpg")
+
+st.image("images/web-penguin.jpg")
